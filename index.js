86c997e051f4a9d9901d9bec67c0223eec43a44c
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-10-02 18:24:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(array1, array2) {
   var newarray = []
   for(var i = 0; i < array1.length; i++) {
-    newarray.push(array1[i] + array2[i])
+    newarray.push(array1[i] + " plays " + array2[i])
   }
   return newarray
 }
